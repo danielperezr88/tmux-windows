1edf8d8b680f09f22716446821bd610675072ab4
danielperezr88 <danielperezr88@gmail.com>
2026-07-04 02:29:05 +0200

    diagnostic: log shell/cmdline/cwd for JOB_PTY spawns

--- job.c
+++ job.c
@@ -140,6 +140,8 @@ job_run(const char *cmd, int argc, char **argv, struct environ *e,
 			xasprintf(&cmdline, "%s /c %s", shell, cmd);
 		else
 			cmdline = cmd_stringify_argv(argc, argv);
+		log_debug("JOB_PTY: shell='%s' cmdline='%s' cwd='%s'", shell,
+		    cmdline, cwd ? cwd : "(null)");
 		pty = win32_pty_spawn(cmdline, cwd, NULL, sx, sy, &pid);
 		free(cmdline);
 		if (pty == NULL)
